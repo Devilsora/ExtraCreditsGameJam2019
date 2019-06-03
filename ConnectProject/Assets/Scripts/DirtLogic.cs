45c178983a9f9f9da64f20f2642eebb49841123a
Josh Marlowe <joshben@cox.net>
2019-06-03 16:09:50 -0700

    Getting Dirt Manager In

---
+++ ConnectProject/Assets/Scripts/DirtLogic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtLogic : MonoBehaviour
+{
+    public bool isDirty;
+    public ParticleSystem cleanParticles;
+    public float particleSystemLifetime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //check if Roomba is above
+      if (isDirty)
+      {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.up, out hit, 1f))
+        {
+          if (hit.transform.gameObject.tag == "Roomba")
+          {
+            isDirty = false;
+          }
+        }
+      }
+      else
+      {
+        
+      }
+      
+    }
+}
---
+++ ConnectProject/Assets/Scripts/DirtManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtManager : MonoBehaviour
+{
+  //dirt spawn info
+  //rotation: x = 90
+  //position: y = -11.11395
+
+  public int numDirt;
+  public GameObject dirt;
+  public AudioSource finishSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
