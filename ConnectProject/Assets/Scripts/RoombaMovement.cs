8fe924a88a5ab9da414ce4c326e4c5e35eaedbe9
Josh Marlowe <joshben@cox.net>
2019-06-03 15:28:48 -0700

    Revert "Merge branch 'RoombaTurnSound'"
    This reverts commit 3ed310af8a7b544fe87d0e4daf392028b21cfc4b.

--- ConnectProject/Assets/Scripts/RoombaMovement.cs
+++ ConnectProject/Assets/Scripts/RoombaMovement.cs
@@ -37,8 +37,6 @@ public class RoombaMovement : MonoBehaviour
   public float distCheck = 1.5f;
   float moveTimer = 0.0f;
 
-  public AudioSource DirectionChangeSound;
-
     // Start is called before the first frame update
     void Start()  
     {
@@ -73,13 +71,8 @@ public class RoombaMovement : MonoBehaviour
 
             if (!checkedNextPositions)
             {
-              Orientation prev = or;
               or = DetermineOrientation();
               checkedNextPositions = true;
-              if(prev != or)
-              {
-                  DirectionChangeSound.Play();
-              }
             }
           }
         }
